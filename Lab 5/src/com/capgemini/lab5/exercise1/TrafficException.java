91939668dc561280b31469e88e06cc435954f558
ramatejareddy <37958422+ramatejareddy@users.noreply.github.com>
2020-02-11 16:31:25 +0530

    lab 5

---
+++ Lab 5/src/com/capgemini/lab5/exercise1/TrafficException.java
@@ -0,0 +1,10 @@
+package com.capgemini.lab5.exercise1;
+
+public class TrafficException extends Exception{
+	public TrafficException(String message)
+	{
+		super(message);
+		//System.out.println(message);
+	}
+
+}
